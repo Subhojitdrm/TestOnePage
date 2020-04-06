33f66cc0078b08264ee616abe0d1d910a51fc10f
Subhojitdrm <55389744+Subhojitdrm@users.noreply.github.com>
2020-04-07 04:31:36 +0530

    S_01_Setup

---
+++ src/main/java/com/cintrelas/App.java
@@ -0,0 +1,13 @@
+package com.cintrelas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
